d4ad6e1d28ee2f606cda7ea419d2ade76d73c38f
Josue <josu.monfer@gmail.com>
2025-05-27 16:33:55 -0600

    submenus corregidos

--- VALIDACION/MenuA.php
+++ VALIDACION/MenuA.php
@@ -31,63 +31,84 @@ if (!isset($_SESSION['username']) || $_SESSION['rol'] !== 'admin') {
     <div class="contenedor">
         <nav class="menu">
             <ul class="lista">
-                <!-- Consultas -->
                 <li class="seccion">
                     <a class="enlaceSeccion">
                         <i class="fas fa-search iconoMenu"></i>
                         Consultas
                     </a>
                     <ul class="submenu">
+                        <li><a href="../SELECT/FSCentrosVerificacion.html"><i class="fas fa-building iconoSubmenu"></i>Centros Verificación</a></li>
+                        <li><a href="../SELECT/FSConductores.html"><i class="fas fa-user iconoSubmenu"></i>Conductores</a></li>
+                        <li><a href="../SELECT/FSDomicilios.html"><i class="fas fa-house-user iconoSubmenu"></i>Domicilios</a></li>
                         <li><a href="../SELECT/FSLicencias.html"><i class="fas fa-id-card iconoSubmenu"></i>Licencias</a></li>
                         <li><a href="../SELECT/FSMultas.html"><i class="fas fa-exclamation-triangle iconoSubmenu"></i>Multas</a></li>
+                        <li><a href="../SELECT/FSOficiales.html"><i class="fas fa-badge iconoSubmenu"></i>Oficiales</a></li>
                         <li><a href="../SELECT/FSPagos.html"><i class="fas fa-credit-card iconoSubmenu"></i>Pagos</a></li>
+                        <li><a href="../SELECT/FSPropietarios.html"><i class="fas fa-id-badge iconoSubmenu"></i>Propietarios</a></li>
                         <li><a href="../SELECT/FSTarjetasCirculacion.html"><i class="fas fa-car-side iconoSubmenu"></i>Tarjetas Circulación</a></li>
                         <li><a href="../SELECT/FSTarjetasVerificacion.html"><i class="fas fa-check-circle iconoSubmenu"></i>Tarjetas Verificación</a></li>
+                        <li><a href="../SELECT/FSVehiculos.html"><i class="fas fa-car iconoSubmenu"></i>Vehículos</a></li>
                     </ul>
                 </li>
-
-                <!-- Registros -->
+                <!-- Consultas -->
                 <li class="seccion">
                     <a class="enlaceSeccion">
                         <i class="fas fa-plus-circle iconoMenu"></i>
                         Registros
                     </a>
                     <ul class="submenu">
+                        <li><a href="../INSERT/FCentrosVerificacion.html"><i class="fas fa-building iconoSubmenu"></i>Centros Verificación</a></li>
+                        <li><a href="../INSERT/FConductores.html"><i class="fas fa-user iconoSubmenu"></i>Conductores</a></li>
+                        <li><a href="../INSERT/FDomicilios.html"><i class="fas fa-house-user iconoSubmenu"></i>Domicilios</a></li>
                         <li><a href="../INSERT/FLicencias.html"><i class="fas fa-id-card iconoSubmenu"></i>Licencias</a></li>
                         <li><a href="../INSERT/FMultas.html"><i class="fas fa-exclamation-triangle iconoSubmenu"></i>Multas</a></li>
+                        <li><a href="../INSERT/FOficiales.html"><i class="fas fa-badge iconoSubmenu"></i>Oficiales</a></li>
                         <li><a href="../INSERT/FPagos.html"><i class="fas fa-credit-card iconoSubmenu"></i>Pagos</a></li>
+                        <li><a href="../INSERT/FPropietarios.html"><i class="fas fa-id-badge iconoSubmenu"></i>Propietarios</a></li>
                         <li><a href="../INSERT/FTarjetasCirculacion.html"><i class="fas fa-car-side iconoSubmenu"></i>Tarjetas Circulación</a></li>
                         <li><a href="../INSERT/FTarjetasVerificacion.html"><i class="fas fa-check-circle iconoSubmenu"></i>Tarjetas Verificación</a></li>
+                        <li><a href="../INSERT/FVehiculos.html"><i class="fas fa-car iconoSubmenu"></i>Vehículos</a></li>
                     </ul>
                 </li>
 
-                <!-- Actualizaciones -->
+                                <!-- Actualizaciones -->
                 <li class="seccion">
                     <a class="enlaceSeccion">
                         <i class="fas fa-edit iconoMenu"></i>
                         Actualizaciones
                     </a>
                     <ul class="submenu">
+                        <li><a href="../UPDATE/BUCentrosVerificacion.html"><i class="fas fa-building iconoSubmenu"></i>Centros Verificación</a></li>
+                        <li><a href="../UPDATE/BUConductores.html"><i class="fas fa-user iconoSubmenu"></i>Conductores</a></li>
+                        <li><a href="../UPDATE/BUDomicilios.html"><i class="fas fa-house-user iconoSubmenu"></i>Domicilios</a></li>
                         <li><a href="../UPDATE/BULicencias.html"><i class="fas fa-id-card iconoSubmenu"></i>Licencias</a></li>
                         <li><a href="../UPDATE/BUMultas.html"><i class="fas fa-exclamation-triangle iconoSubmenu"></i>Multas</a></li>
+                        <li><a href="../UPDATE/BUOficiales.html"><i class="fas fa-badge iconoSubmenu"></i>Oficiales</a></li>
                         <li><a href="../UPDATE/BUPagos.html"><i class="fas fa-credit-card iconoSubmenu"></i>Pagos</a></li>
+                        <li><a href="../UPDATE/BUPropietarios.html"><i class="fas fa-id-badge iconoSubmenu"></i>Propietarios</a></li>
                         <li><a href="../UPDATE/BUTarjetasCirculacion.html"><i class="fas fa-car-side iconoSubmenu"></i>Tarjetas Circulación</a></li>
                         <li><a href="../UPDATE/BUTarjetasVerificacion.html"><i class="fas fa-check-circle iconoSubmenu"></i>Tarjetas Verificación</a></li>
+                        <li><a href="../UPDATE/BUVehiculos.html"><i class="fas fa-car iconoSubmenu"></i>Vehículos</a></li>
                     </ul>
                 </li>
 
-                <!-- Eliminaciones -->
                 <li class="seccion">
                     <a class="enlaceSeccion">
                         <i class="fas fa-trash-alt iconoMenu"></i>
                         Eliminaciones
                     </a>
                     <ul class="submenu">
+                        <li><a href="../DELETE/FDCentrosVerificacion.html"><i class="fas fa-building iconoSubmenu"></i>Centros Verificación</a></li>
+                        <li><a href="../DELETE/FDConductores.html"><i class="fas fa-user iconoSubmenu"></i>Conductores</a></li>
+                        <li><a href="../DELETE/FDDomicilios.html"><i class="fas fa-house-user iconoSubmenu"></i>Domicilios</a></li>
                         <li><a href="../DELETE/FDLicencias.html"><i class="fas fa-id-card iconoSubmenu"></i>Licencias</a></li>
                         <li><a href="../DELETE/FDMultas.html"><i class="fas fa-exclamation-triangle iconoSubmenu"></i>Multas</a></li>
+                        <li><a href="../DELETE/FDOficiales.html"><i class="fas fa-badge iconoSubmenu"></i>Oficiales</a></li>
                         <li><a href="../DELETE/FDPagos.html"><i class="fas fa-credit-card iconoSubmenu"></i>Pagos</a></li>
-                        <li><a href="../DELETE/FDTarjetasCirculacion.html"><i class="fas fa-car-side iconoSubmenu"></i>Tarjetas Circulación</a></li>
+                        <li><a href="../DELETE/FDPropietarios.html"><i class="fas fa-id-badge iconoSubmenu"></i>Propietarios</a></li>
+                        <li><a href="../DELETE/FDarjetasCirculacion.html"><i class="fas fa-car-side iconoSubmenu"></i>Tarjetas Circulación</a></li>
                         <li><a href="../DELETE/FDTarjetasVerificacion.html"><i class="fas fa-check-circle iconoSubmenu"></i>Tarjetas Verificación</a></li>
+                        <li><a href="../DELETE/FDVehiculos.html"><i class="fas fa-car iconoSubmenu"></i>Vehículos</a></li>
                     </ul>
                 </li>
             </ul>
--- VALIDACION/MenuU.php
+++ VALIDACION/MenuU.php
@@ -32,11 +32,17 @@ if (!isset($_SESSION['username']) || $_SESSION['rol'] !== 'usuario') {
                         Consultas
                     </a>
                     <ul class="submenu">
+                        <li><a href="../SELECT/FSCentrosVerificacion.html"><i class="fas fa-building iconoSubmenu"></i>Centros Verificación</a></li>
+                        <li><a href="../SELECT/FSConductores.html"><i class="fas fa-user iconoSubmenu"></i>Conductores</a></li>
+                        <li><a href="../SELECT/FSDomicilios.html"><i class="fas fa-house-user iconoSubmenu"></i>Domicilios</a></li>
                         <li><a href="../SELECT/FSLicencias.html"><i class="fas fa-id-card iconoSubmenu"></i>Licencias</a></li>
-                        <li><a href="../SELECT/FSTarjetasCirculacion.html"><i class="fas fa-car-side iconoSubmenu"></i>Tarjetas Circulación</a></li>
                         <li><a href="../SELECT/FSMultas.html"><i class="fas fa-exclamation-triangle iconoSubmenu"></i>Multas</a></li>
-                        <li><a href="../SELECT/FSTarjetasVerificacion.html"><i class="fas fa-check-circle iconoSubmenu"></i>Tarjetas Verificación</a></li>
+                        <li><a href="../SELECT/FSOficiales.html"><i class="fas fa-badge iconoSubmenu"></i>Oficiales</a></li>
                         <li><a href="../SELECT/FSPagos.html"><i class="fas fa-credit-card iconoSubmenu"></i>Pagos</a></li>
+                        <li><a href="../SELECT/FSPropietarios.html"><i class="fas fa-id-badge iconoSubmenu"></i>Propietarios</a></li>
+                        <li><a href="../SELECT/FSTarjetasCirculacion.html"><i class="fas fa-car-side iconoSubmenu"></i>Tarjetas Circulación</a></li>
+                        <li><a href="../SELECT/FSTarjetasVerificacion.html"><i class="fas fa-check-circle iconoSubmenu"></i>Tarjetas Verificación</a></li>
+                        <li><a href="../SELECT/FSVehiculos.html"><i class="fas fa-car iconoSubmenu"></i>Vehículos</a></li>
                     </ul>
                 </li>
             </ul>
